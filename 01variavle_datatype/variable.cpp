5a42b43322dc295cf288e2d84208ae69644c1888
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-08-02 00:22:56 +0530

    unary operator

--- 01variavle_datatype/variable.cpp
+++ 01variavle_datatype/variable.cpp
@@ -16,11 +16,29 @@ int main()
   // cin >> prise;
   // cout << "The Prise is : " << prise;
 
-  int a = 10, b = 5;
-  cout << "sum is = " << (a + b) << endl;
-  cout << "difference is = " << (a - b) << endl;
-  cout << "product is = " << (a * b) << endl;
-  cout << "division is = " << (a / b) << endl;
-  cout << "modulo is = " << (a % b) << endl;
+  // int a = 10, b = 5;
+  // cout << "sum is = " << (a + b) << endl;
+  // cout << "difference is = " << (a - b) << endl;
+  // cout << "product is = " << (a * b) << endl;
+  // cout << "division is = " << (a / b) << endl;
+  // cout << "modulo is = " << (a % b) << endl;
+
+  // cout << (3 > 5);  // false = 0
+  // cout << (3 < 5);  // true = 1
+  // cout << (3 >= 5); // false = 0
+  // cout << (3 <= 5); // true = 1
+  // cout << (3 == 5); // false = 0
+  // cout << (3 != 5); // true = 1
+
+  // cout << !(1 < 3);
+
+  // sum of two number
+  int a, b;
+  cout << "Enter A value : ";
+  cin >> a;
+  cout << "Enter B value ";
+  cin >> b;
+  int sum = a + b;
+  cout << "Sum of A and B is : " << sum << endl;
   return 0;
 }
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 # Today is the first day of learning DSA with C++
 
-<h1>To day i am learn about : </h1>
+<h1>Date - 01 / 08 / 2025</h1>
+<h2>To day i am learn about : </h2>
 <ul>
 <li>Variables</li>
 <li>Data Types</li>
 <li>Input & Output</li>
 <li>Operators</li>
+<li>Unary Operator </li>
 </ul>
