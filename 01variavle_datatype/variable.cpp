9ff162a7672e7d3d410f5a8ff068b4193acf3443
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-08-01 23:53:55 +0530

    day one learning c++

---
+++ 01variavle_datatype/variable.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int age;
+  // char grade = "A";
+  float price = 100.33f;
+  // cout << price << endl;
+  // cout << "Enter your age:";
+  // cin >> age;
+  // cout << "Your age is : " << age << endl;
+  // int prise;
+  // cout << "Enter the Price : ";
+  // cin >> prise;
+  // cout << "The Prise is : " << prise;
+
+  int a = 10, b = 5;
+  cout << "sum is = " << (a + b) << endl;
+  cout << "difference is = " << (a - b) << endl;
+  cout << "product is = " << (a * b) << endl;
+  cout << "division is = " << (a / b) << endl;
+  cout << "modulo is = " << (a % b) << endl;
+  return 0;
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Today is the first day of learning DSA with C++
+
+<h1>To day i am learn about : </h1>
+<ul>
+<li>Variables</li>
+<li>Data Types</li>
+<li>Input & Output</li>
+<li>Operators</li>
+</ul>
